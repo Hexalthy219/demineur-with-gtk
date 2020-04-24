aab798b063e363eae7c0d348f6b7da2982b5d463
Cyril Russe <cyril.russe@student.uliege.be>
2020-04-24 16:07:27 +0200

    Test mise en commun

--- source/controleur_demineur.c
+++ source/controleur_demineur.c
@@ -7,5 +7,26 @@
  * \date: 24-04-2020
  * 
  */
+#include <stdlib.h>
+#include <assert.h>
 
 #include "controleur_demineur.h"
+
+short **cree_tableau2D(int ligne, int colonne){
+    assert(ligne>0 && colonne>0);
+
+    short **tableau = malloc(ligne*short);
+    if(tableau==NULL)
+        return NULL;
+    
+    for(int i=0; i<ligne, i++){
+        tableau[i] = malloc(colonne*short);
+        if(tableau[i]==NULL){
+            for(--i; i>=0; i--)
+                free(tableau[i]);
+            free(tableau);
+            return NULL;
+        }
+    }
+    return tableau;
+}//fin cree_tableau2D
