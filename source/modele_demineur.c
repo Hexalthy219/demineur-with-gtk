c32de931c7c310112c48ac316406cd9d2dc48101
Martin <Equynoxe>
2020-04-25 15:58:18 +0200

    bombe aléatoire

--- source/modele_demineur.c
+++ source/modele_demineur.c
@@ -9,13 +9,27 @@
  */
 
 #include "modele_demineur.h"
-#include "type_opaque.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 
-// void aleatoire_bombe(Terrain *terrain_de_jeu){
-//     srand(time(NULL));
-//     for(int i = 0; i< terrain_de_jeu)
-// }
+void aleatoire_bombe(Terrain *terrain_de_jeu){
+    srand(time(NULL));
+    unsigned short nbr_mine = get_nombre_mine(get_regle(terrain_de_jeu));
+    unsigned short colonne_max = get_ligne(get_regle(terrain_de_jeu));
+    unsigned short ligne_max = get_colonne(get_regle(terrain_de_jeu));
+
+    
+
+    for(unsigned short k = 0; k < nbr_mine; k++){
+        unsigned short i = rand()%ligne_max;
+        unsigned short j = rand()%colonne_max;
+        if(get_mine(get_elem_champ_mine(terrain_de_jeu, i, j)) == 0)
+            --k;
+        else{
+            set_mine(get_elem_champ_mine(terrain_de_jeu, i, j), -1);
+        }
+        
+    }
+}
